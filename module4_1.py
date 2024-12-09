fcec83d1006579ac56bc87fd1b19ff9c743529ae
SergK <sergkom26@gmail.com>
2024-12-09 17:31:46 +0300

    Lesson1

---
+++ module4_1.py
@@ -0,0 +1,13 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+
+results_1 = fm(69, 3)
+results_2 = fm(3, 0)
+results_3 = tm(49, 7)
+results_4 = tm(15, 0)
+
+print(results_1)
+print(results_2)
+print(results_3)
+print(results_4)
